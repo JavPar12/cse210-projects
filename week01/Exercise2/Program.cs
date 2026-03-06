02e63e9a6d5a203873cd7a67e84ebffdfb5d3178
Luis <pardojavier12@gmail.com>
2026-03-05 21:55:08 -0600

    Core and Stretch requirements completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+
+        int grade = int.Parse(userInput);
+
+        string letter = "";
+        string sign = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int lastDigit = grade %10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else
+        {
+            sign = "-";
+        }
+        if (letter == "A" && sign == "+")
+        {
+            sign="";
+        }
+        if (letter == "F" && sign == "-")
+        {
+            sign="";
+        }
+        Console.WriteLine($"Your Grade is {letter}{sign}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations your passed the course.!");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! try your best next time.");
+        }
     }
 }
