fbd9461c10ba2349ac50a5e0d6a2dcd019ff3b73
Luis <pardojavier12@gmail.com>
2026-03-05 19:09:13 -0600

    Name exercise properly done

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your Name? ");
+        string firstname = Console.ReadLine();
+
+        Console.Write("What is your Last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your Name is {lastname}, {firstname} {lastname}.");
     }
 }
